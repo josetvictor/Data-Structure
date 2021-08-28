5f59f1529d938ff9f7d124fbc8d7dc6fa448da6a
José Victor <zevicctor2@gmail.com>
2021-08-28 12:53:20 -0300

    Insert verificando os 2 primeiros casos e rotações da arvore

---
+++ structs/Arvore/TreeRN.cs
@@ -0,0 +1,190 @@
+using data_structs.Node;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data_structs.Arvore
+{
+    class TreeRN : SearchBinaryTree
+    {
+        private NoRubroNegra root;
+        private int lenght;
+
+        public TreeRN(NoRubroNegra no) : base(no)
+        {
+            root = no;
+            lenght = 1;
+        }
+
+        public int depthRN(NoRubroNegra node)
+        {
+            if (node == null || node == root)
+                return 0;
+            else
+                return (1 + depthRN(node.Parent()));
+        }
+
+        public int heightRN(NoRubroNegra node)
+        {
+            if (node == null || (node.Left() == null && node.Right() == null))
+                return 0;
+            else
+            {
+                if (heightRN(node.Left()) > heightRN(node.Right()))
+                    return 1 + heightRN(node.Left());
+                else 
+                    return 1 + heightRN(node.Right());
+            }
+        }
+
+        public void showTreeRN(NoRubroNegra tree)
+        {
+            int rows = heightRN(tree) + 1;
+            int columns = lenght;
+
+            NoRubroNegra[,] matrizTree = new NoRubroNegra[rows, columns];
+            ArrayList nodes = new ArrayList();
+
+            nodes = tree.nodeElements(nodes, tree);
+
+            nodes.ToArray();
+            for(int i = 0; i < columns; i++)
+            {
+                NoRubroNegra no = null;
+                if (i < nodes.ToArray().Length)
+                    no = (NoRubroNegra)nodes[i]; ;
+                matrizTree[depthRN(no), i] = no;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    NoRubroNegra node = matrizTree[i, j];
+
+                    if (node == null)
+                        Console.Write(" ");
+                    else
+                        Console.Write(node.Element() + "[" + node.Cor() + "]");
+
+                    Console.Write("   ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public NoRubroNegra rotationSL(NoRubroNegra node)
+        {
+            var vovz = node.Parent();
+            var aux = node;
+            node = node.Right();
+            node.setParent(aux.Parent());
+            aux.setParent(node);
+            node.setLeft(aux);
+            aux.setRight(node.Left());
+            node.setParent(vovz);
+
+            if (vovz.Left() == aux)
+                vovz.setLeft(node);
+            else
+                vovz.setRight(node);
+
+            node.setCor('R');
+            node.Left().setCor('N');
+            node.Right().setCor('N');
+            return node;
+        }
+
+        public NoRubroNegra rotationSR(NoRubroNegra node)
+        {
+            var vovz = node.Parent();
+            var aux = node;
+            node = node.Left();
+            node.setParent(aux.Parent());
+            aux.setParent(node);
+            node.setRight(aux);
+            aux.setLeft(node.Right());
+            node.setParent(vovz);
+
+            if (vovz.Left() == aux)
+                vovz.setLeft(node);
+            else
+                vovz.setRight(node);
+
+            node.setCor('R');
+            node.Left().setCor('N');
+            node.Right().setCor('N');
+            return node;
+        }
+
+        public void recolorir(NoRubroNegra node){
+            if(node.Parent() == null && node.Cor() == "N")
+                return;
+            else if(node != null && node.Cor() == "R")
+                node.setCor("N");
+            else if(node != null && node.Cor() == "N")
+                node.setCor("R");
+        }
+
+        public void insertRN(NoRubroNegra node, int value)
+        {
+            // Percorre a arvore e insere o nó como na BST
+            if (node != null)
+            {
+                if (value > node.Element())
+                {
+                    if (node.Right() == null)
+                    {
+                        node.setRight(new NoRubroNegra(node, value));
+                        lenght++;
+                        if(node.Parent().Cor() == "R")
+                            recolorir(node);
+                    }
+                    else
+                        insert(node.Right(), value);
+                }
+                else
+                {
+                    if (node.Left() == null)
+                    {
+                        node.setLeft(new NoRubroNegra(node, value));
+                        lenght++;
+                        if(node.Parent().Cor() == "R")
+                            recolorir(node);
+                    }
+                    else insert(node.Left(), value);
+                }
+                balanceRubroNegra(NoRubroNegra node);
+            }
+        }
+
+        public void balanceRubroNegra(NoRubroNegra node)
+        {
+            // caso 1
+            if(node.Cor() == "R" && node.Parent().Cor() == "N")
+                return;
+            // caso 2
+            if(node.Cor() == "R" && node.Parent().Cor() == "R"){
+                // Verifica quem é o tio e salva em uma variavel
+                var uncle;
+                if(node.Parent() == node.Parent().Parent().Left())
+                    uncle = node.Parent().Parent().Right();
+                else
+                    uncle = node.Parent().Parent().Left();
+
+                if(uncle != null && uncle == "R"){
+                    recolorir(node.Parent());
+                    recolorir(node.Parent().Parent());
+                    recolorir(node.Parent().Parent().Left());
+                } else if(uncle != null && uncle == "R"){
+                    recolorir(node.Parent());
+                    recolorir(node.Parent().Parent());
+                    recolorir(node.Parent().Parent().Right());
+                }
+                balanceRubroNegra(node.Parent().Parent());
+            }
+            // caso 3
+        }
+    }
+}
---
+++ structs/Node/NoRubroNegra.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+namespace data_structs.Node
+{
+    public class NoRubroNegra
+    {
+        private int element;
+        private string cor;
+        private NoRubroNegra dad;
+        private NoRubroNegra left;
+        private NoRubroNegra right;
+
+        public NoRubroNegra(int element)
+        {
+            this.dad = null;
+            this.element = element;
+            cor = "N";
+            left = null;
+            right = null;
+        }
+
+        public NoRubroNegra(NoRubroNegra dad, int element)
+        {
+            this.dad = dad;
+            this.element = element;
+            Cor - "R";
+            left = null;
+            right = null;
+        }
+
+        public string Cor() => Cor;
+
+        public string setCor(string cor) => this.cor = cor;
+
+        public int Element() => element;
+
+        public void setElement(int element) => this.element = element;
+
+        public NoRubroNegra Parent() => dad;
+
+        public void setParent(NoRubroNegra node) => dad = node;
+
+        public NoRubroNegra Left() => left;
+
+        public void setLeft(NoRubroNegra node) => left = node;
+
+        public void removeLeft() => left = null;
+
+        public NoRubroNegra Right() => right;
+
+        public void setRight(NoRubroNegra node) => right = node;
+
+        public void removeRight() => right = null;
+
+        public int childrenNumber()
+        {
+            int children = 0;
+            if (left != null) children++;
+            if (right != null) children++;
+
+            return children;
+        }
+
+        public IEnumerable children()
+        {
+            List<NoRubroNegra> list = new List<NoRubroNegra>();
+
+            if (left != null) list.Add(left);
+            if (right!= null) list.Add(right);
+
+            yield return list.ToArray().GetEnumerator();
+        }
+
+        public ArrayList nodeElements(ArrayList nodes, NoRubroNegra no)
+        {
+            NoRubroNegra leftSon = no.left;
+            NoRubroNegra rightSon = no.right;
+
+            if(no != null)
+            {
+                if(leftSon != null)
+                    nodeElements(nodes, leftSon);
+
+                nodes.Add(no);
+
+                if(rightSon != null)
+                    nodeElements(nodes, rightSon);
+            }
+
+            return nodes;
+        }
+    }
+}
